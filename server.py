c7819a595be15e8917bf2fe7587c8ddc39e49a1c
Brian Peiris <brianpeiris@gmail.com>
2017-06-30 00:11:23 -0700

    fix server characters

--- server.py
+++ server.py
@@ -44,21 +44,21 @@ button_state_to_key_index = [
 normal_keys = [
     "NULL", "a", "b", "c", "d","e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", 
     "v", "w", "x", "y", "z", "th", "that ", "the ", "of ", 
-    ".", ",", "!", "?", "-", "\'", "\\", "/", "and ", "§", "to ", 
+    ".", ",", "!", "?", "-", "\'", "\\", "/", "and ", None, "to ", 
     "{UP}", "{DOWN}", "{PGUP}", "{PGDN}", "{BACKSPACE}", "{LEFT}", "^{LEFT}", "{HOME}", "{SPACE}", "{RIGHT}", "^{RIGHT}", "{END}", 
     "{ENTER}", "{TAB}", "{ESC}", "{DEL}", "{INS}", "_SHIFT_", "_SYMBOL_", None, "{VK_CONTROL}", "{VK_MENU}" 
 ]
 shift_keys = [
     "NULL", "A", "B", "C", "D","E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", 
     "V", "W", "X", "Y", "Z", "Th", "That ", "The ", "Of ", 
-    ":", ";", "|", "{~}", "_", "\"", "`", "/", "And ", "§", "To ",
+    ":", ";", "|", "{~}", "_", "\"", "`", "/", "And ", None, "To ",
     "{UP}", "{DOWN}", "{PGUP}", "{PGDN}", "{BACKSPACE}", "{LEFT}", "^{LEFT}", "{HOME}", "{SPACE}", "{RIGHT}", "^{RIGHT}", "{END}", 
     "{ENTER}", "{TAB}", "{ESC}", "{DEL}", "{INS}", "_NORMAL_", "_SYMBOL_", None, "{VK_CONTROL}", "{VK_MENU}" 
 ]
 symbol_keys = [
-    "NULL", "1", "2", "3", "4","5", "6", "0", "7", "8", "9", "#", "@", "½", "&", "{+}", "{%}", "=", "{^}", "*", "$", "€", 
-    "£", "{(}", "[", "<", "{{}", "{)}", "]", ">", "{}}", 
-    ":", ";", "|", "{~}", "_", "\"", "`", "´", "μ", "§", "", 
+    "NULL", "1", "2", "3", "4","5", "6", "0", "7", "8", "9", "#", "@", None, "&", "{+}", "{%}", "=", "{^}", "*", "$", None, 
+    None, "{(}", "[", "<", "{{}", "{)}", "]", ">", "{}}", 
+    ":", ";", "|", "{~}", "_", "\"", "`", None, None, None, None, 
     "{UP}", "{DOWN}", "{PGUP}", "{PGDN}", "{BACKSPACE}", "{LEFT}", "^{LEFT}", "{HOME}", "{SPACE}", "{RIGHT}", "^{RIGHT}", "{END}", 
     "{ENTER}", "{TAB}", "{ESC}", "{DEL}", "{INS}", "_SHIFT_", "_NORMAL_", None, "{VK_CONTROL}", "{VK_MENU}" 
 ]
@@ -89,7 +89,8 @@ if __name__ == "__main__":
                 else:
                     print(last_key, end='')
                     SendKeys(last_key)
-                    sys.stdout.flush()
+                    mode = '_NORMAL_'
+                sys.stdout.flush()
             last_key = None
             last_val = 0
             continue
